7c3a527fa16e31b37b77c60e7c428054d1fbd38e
erezfree29 <erezfree29@gmail.com>
2020-03-19 09:30:52 -0400

    code refactored for Buy 3 get 1 free for mangos

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -27,12 +27,18 @@ class Checkout
         else
           total += (prices.fetch(item) / 2) * count
         end
+      # 1st commit
       # buy 3 get 1 free solution for mangos
       # item check in basket if it is mango then count checked. 
       # if it is greater than 3, discount is added by reducing one manago in total price
+      
+      # 2nd commit
+      # code is refactored and discounted mangos count using mode.
+      # In every 3 mangos 1 is added free in cart
       elsif item == :mango
-        if (count >= 3)
-          total += prices.fetch(item) * (count - 1)
+        discount_items = count % 3
+        if (discount_items >= 0)
+          total += prices.fetch(item) * (count - discount_items)
         end
       else
         total += prices.fetch(item) * count
